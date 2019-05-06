36d601c005b3f266f1c4e633c811d6dfc2c662e8
Yuriy <uapetrovsky@gmail.com>
2019-05-06 13:08:38 +0300

    Update

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,9 @@
 add-apt-repository universe
 add-apt-repository ppa:far2l-team/ppa
 apt update
-apt -y install gnome-tweaks chrome-gnome-shell vim mc htop git tmux fish far2l
+apt -y install gnome-tweaks chrome-gnome-shell vim mc htop git tmux curl fish far2l
 snap install go --classic
 snap install chromium telegram-desktop
+curl -L https://get.oh-my.fish | fish
+cp -rf .tmux.conf ~/.tmux.conf
+cp -rf .vimrc ~/.vimrc
